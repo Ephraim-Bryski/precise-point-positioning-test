acaff64720f95c1f575a10140efb1809604d4572
Ephraim <ebryski1@gmail.com>
2025-03-12 12:02:03 -0400

    cleaned code

--- create_maps.py
+++ create_maps.py
@@ -3,11 +3,11 @@ import folium
 import pandas as pd
 
 
-def create_map(plot_coordinates, map_filename):
+def create_map(latitudes, longitudes, map_filename):
 
     """saves a map with the latitude, longitude coordinates plotted"""
 
-    latitudes, longitudes = zip(*plot_coordinates)
+    plot_coordinates = zip(latitudes, longitudes)
 
     m = folium.Map(tiles='Esri.WorldImagery')
     min_lat = min(latitudes)
@@ -22,10 +22,8 @@ def create_map(plot_coordinates, map_filename):
 
 # this is the real-time gps data from sensorlogger
 gps_output = pd.read_csv(r"sensorlogger output\Location.csv")
-gps_coordinates = list(zip(gps_output.latitude, gps_output.longitude))
-create_map(gps_coordinates, 'gps_map.html')
+create_map(gps_output.latitude, gps_output.longitude, 'gps_map.html')
 
 # this is the post-processed ppp data from https://webapp.csrs-scrs.nrcan-rncan.gc.ca/geod/tools-outils/ppp.php
 ppp_output = pd.read_csv(r"rncan full_output\gnss_log_2025_02_26_09_01_38.csv")
-ppp_coordinates = list(zip(ppp_output.latitude_decimal_degree, ppp_output.longitude_decimal_degree))
-create_map(ppp_coordinates, 'ppp_map.html')
+create_map(ppp_output.latitude_decimal_degree, ppp_output.longitude_decimal_degree, 'ppp_map.html')
